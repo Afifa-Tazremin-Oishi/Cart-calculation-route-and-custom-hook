fa55884277f0042f39aedfa6171f58ef47a3ea56
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-07-30 03:18:39 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>I am about component....</h1>
+            <p>I want to be secret...</p>
+        </div>
+    );
+};
+
+export default About;
